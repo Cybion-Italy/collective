9a235cb799735bcbde246b8b8f3108177cf0266f
Matteo Moci <mox601@libero.it>
2013-05-06 17:51:03 +0200

    updated api to use commons sparql-proxy-api

--- recommender/core/src/main/java/com/collective/recommender/proxyimpl/SesameVirtuosoSparqlProxy.java
+++ recommender/core/src/main/java/com/collective/recommender/proxyimpl/SesameVirtuosoSparqlProxy.java
@@ -2,11 +2,12 @@ package com.collective.recommender.proxyimpl;
 
 import com.collective.recommender.*;
 import com.collective.recommender.configuration.RecommenderConfiguration;
-import com.collective.recommender.proxy.SparqlProxy;
+import com.collective.recommender.proxy.BaseSesameSparqlProxy;
 import com.collective.recommender.proxy.SparqlProxyException;
 import com.collective.recommender.proxy.SparqlQuery;
 import com.collective.recommender.proxy.filtering.Filter;
 import com.collective.recommender.proxy.filtering.FilterException;
+import com.collective.recommender.proxy.queries.QueryRecord;
 import com.collective.recommender.proxy.ranking.Ranker;
 import com.collective.recommender.proxy.ranking.RankerException;
 import org.apache.log4j.Logger;
@@ -28,7 +29,7 @@ import java.util.Map;
  * @author Davide Palmisano ( dpalmisano@gmail.com )
  * @author Matteo Moci (matteo (dot) moci (at) gmail (dot) com)
  */
-public class SesameVirtuosoSparqlProxy implements SparqlProxy {
+public class SesameVirtuosoSparqlProxy extends BaseSesameSparqlProxy {
 
     private static final Logger LOGGER = Logger.getLogger(SesameVirtuosoSparqlProxy.class);
 
@@ -44,8 +45,8 @@ public class SesameVirtuosoSparqlProxy implements SparqlProxy {
 
     public SesameVirtuosoSparqlProxy() {}
 
-    public SesameVirtuosoSparqlProxy(RecommenderConfiguration recommenderConfiguration,
-                                     Be3 rdFizer) {
+    public SesameVirtuosoSparqlProxy(final RecommenderConfiguration recommenderConfiguration,
+                                     final Be3 rdFizer) {
         this.recommenderConfiguration = recommenderConfiguration;
         this.usersCustomAnnotationsTemplate = this.recommenderConfiguration
                                         .getIndexes().get("custom-annotations");
@@ -71,21 +72,21 @@ public class SesameVirtuosoSparqlProxy implements SparqlProxy {
         );
     }
 
-    @Override
-    public void registerQuery(
-            String id,
-            String queryTemplate,
-            SparqlQuery.TYPE queryType,
-            Class<? extends Filter> filter,
-            Class<? extends Ranker> ranker)
-            throws SparqlProxyException {
-        if (queries.containsKey(id)) {
-            final String errMsg = "Error: query with id: '" + id + "' is already registered";
-            LOGGER.error(errMsg);
-            throw new SparqlProxyException(errMsg);
-        }
-        queries.put(id, new QueryRecord(queryTemplate, queryType, filter, ranker));
-    }
+//    @Override
+//    public void registerQuery(
+//            String id,
+//            String queryTemplate,
+//            SparqlQuery.TYPE queryType,
+//            Class<? extends Filter> filter,
+//            Class<? extends Ranker> ranker)
+//            throws SparqlProxyException {
+//        if (queries.containsKey(id)) {
+//            final String errMsg = "Error: query with id: '" + id + "' is already registered";
+//            LOGGER.error(errMsg);
+//            throw new SparqlProxyException(errMsg);
+//        }
+//        queries.put(id, new QueryRecord(queryTemplate, queryType, filter, ranker));
+//    }
 
     @Override
     public <T> List<T> getList(String id, T returnType, String... args) throws SparqlProxyException {
@@ -94,34 +95,132 @@ public class SesameVirtuosoSparqlProxy implements SparqlProxy {
             LOGGER.error(errMsg);
             throw new SparqlProxyException(errMsg);
         }
-        String queryTemplate = queries.get(id).getQueryTemplate();
-        Filter filter;
+        final QueryRecord queryRecord = queries.get(id);
+        final String queryTemplate = queryRecord.getQueryTemplate();
+        final String queryInstance = valorizeQuery(queryTemplate, args);
+        final RepositoryConnection repositoryConnection = getConnection();
+        final Query query = getQuery(queryRecord, queryInstance, repositoryConnection);
+        final Filter filter = getFilter(queryRecord);
+        final List<Statement> statements = getStatements(queryRecord, filter, query);
+        releaseConnection(repositoryConnection);
+        final List<T> unorderedObjects = deserializeObjects(filter, statements);
+        final Ranker ranker = getRanker(queryRecord);
+        return rankObjects(ranker, unorderedObjects);
+    }
+
+    private <T> List<T> rankObjects(Ranker ranker, List<T> unorderedObjects) throws SparqlProxyException {
+
         try {
-            filter = queries.get(id).getFilter().newInstance();
-        } catch (InstantiationException e) {
-            final String errMsg = "Error while instantiating filter";
-            LOGGER.error(errMsg, e);
-            throw new SparqlProxyException(errMsg, e);
-        } catch (IllegalAccessException e) {
-            final String errMsg = "Error while accessing to filter";
+            return ranker.rank(unorderedObjects);
+        } catch (RankerException e) {
+            final String errMsg = "Error while ranking query results";
             LOGGER.error(errMsg, e);
             throw new SparqlProxyException(errMsg, e);
         }
-        Ranker ranker;
+    }
+
+    private <T> List<T> deserializeObjects(Filter filter, List<Statement> statements)
+            throws SparqlProxyException {
+
+        final List<T> unorderedObjects;
         try {
-            ranker = queries.get(id).getRanker().newInstance();
-        } catch (InstantiationException e) {
-            final String errMsg = "Error while instantiating the provided ranker";
-            throw new SparqlProxyException(errMsg, e);
-        } catch (IllegalAccessException e) {
-            final String errMsg = "Error while accessing the provided ranked";
+            unorderedObjects = filter.getObjects(statements, this.rdfizer);
+        } catch (FilterException e) {
+            final String errMsg = "Error while filtering query results";
+            LOGGER.error(errMsg, e);
             throw new SparqlProxyException(errMsg, e);
         }
-        String queryInstance = valorizeQuery(queryTemplate, args);
-        RepositoryConnection repositoryConnection = getConnection();
+        return unorderedObjects;
+    }
+
+    private List<Statement> getStatements(QueryRecord queryRecord, Filter filter, Query query)
+            throws SparqlProxyException {
+
+        List<Statement> statements = new ArrayList<Statement>();
+        if(queryRecord.getQueryType().equals(SparqlQuery.TYPE.TUPLE)) {
+            final TupleQueryResult tupleQueryResult;
+            try {
+                tupleQueryResult = ((TupleQuery) query).evaluate();
+            } catch (QueryEvaluationException e) {
+                final String errMsg = "Error during the evaluation of the SPARQL query";
+                LOGGER.error(errMsg, e);
+                throw new SparqlProxyException(errMsg, e);
+            }
+            statements = getStatementsFromTuples(queryRecord, filter, tupleQueryResult);
+            closeTupleResult(tupleQueryResult);
+        } else if(queryRecord.getQueryType().equals(SparqlQuery.TYPE.GRAPH)) {
+            final GraphQueryResult graphQueryResult;
+            try {
+                graphQueryResult = ((GraphQuery) query).evaluate();
+            } catch (QueryEvaluationException e) {
+                final String errMsg = "Error during the evaluation of the SPARQL query";
+                LOGGER.error(errMsg, e);
+                throw new SparqlProxyException(errMsg, e);
+            }
+            statements = getStatementsFromGraph(queryRecord, filter, graphQueryResult);
+            closeGraphResult(graphQueryResult);
+        }
+        return statements;
+    }
+
+    private void closeTupleResult(TupleQueryResult queryResult) throws SparqlProxyException {
+
+        try {
+            queryResult.close();
+        } catch (QueryEvaluationException e) {
+            final String errMsg = "Error while closing queryresult";
+            LOGGER.error(errMsg, e);
+            throw new SparqlProxyException(errMsg, e);
+        }
+    }
+
+    private void closeGraphResult(GraphQueryResult queryResult) throws SparqlProxyException {
+
+        try {
+            queryResult.close();
+        } catch (QueryEvaluationException e) {
+            final String errMsg = "Error while closing queryresult";
+            LOGGER.error(errMsg, e);
+            throw new SparqlProxyException(errMsg, e);
+        }
+    }
+
+    private List<Statement> getStatementsFromGraph(QueryRecord queryRecord, Filter filter,
+                                                   GraphQueryResult queryResult)
+            throws SparqlProxyException {
+
+        List<Statement> statements;
+        try {
+            statements = filter.getStatements(queryResult, queryRecord.getQueryType());
+        } catch (FilterException e) {
+            final String errMsg = "Error while inspecting the query result";
+            LOGGER.error(errMsg, e);
+            throw new SparqlProxyException(errMsg, e);
+        }
+        return statements;
+    }
+
+    private List<Statement> getStatementsFromTuples(QueryRecord queryRecord, Filter filter,
+                                                    TupleQueryResult queryResult)
+            throws SparqlProxyException {
+
+        List<Statement> statements;
+        try {
+            statements = filter.getStatements(queryResult, queryRecord.getQueryType());
+        } catch (FilterException e) {
+            final String errMsg = "Error while inspecting the query result";
+            LOGGER.error(errMsg, e);
+            throw new SparqlProxyException(errMsg, e);
+        }
+        return statements;
+    }
+
+    private Query getQuery(QueryRecord queryRecord, String queryInstance,
+                           RepositoryConnection repositoryConnection) throws SparqlProxyException {
+
         Query query;
         try {
-            query = queries.get(id).prepareQuery(repositoryConnection, queryInstance);
+            query = queryRecord.prepareQuery(repositoryConnection, queryInstance);
         } catch (RepositoryException e) {
             final String errMsg = "Error while accessing to the triple store";
             LOGGER.error(errMsg, e);
@@ -131,66 +230,39 @@ public class SesameVirtuosoSparqlProxy implements SparqlProxy {
             LOGGER.error(errMsg, e);
             throw new SparqlProxyException(errMsg, e);
         }
-        List<Statement> statements = new ArrayList<Statement>();
-        if(queries.get(id).queryType.equals(SparqlQuery.TYPE.TUPLE)) {
-            TupleQueryResult queryResult;
-            try {
-                queryResult = ((TupleQuery) query).evaluate();
-            } catch (QueryEvaluationException e) {
-                final String errMsg = "Error during the evaluation of the SPARQL query";
-                LOGGER.error(errMsg, e);
-                throw new SparqlProxyException(errMsg, e);
-            }
-            try {
-                statements = filter.getStatements(queryResult, queries.get(id).queryType);
-            } catch (FilterException e) {
-                final String errMsg = "Error while inspecting the query result";
-                LOGGER.error(errMsg, e);
-                throw new SparqlProxyException(errMsg, e);
-            }
-            try {
-                queryResult.close();
-            } catch (QueryEvaluationException e) {
-                final String errMsg = "Error while closing queryresult";
-                LOGGER.error(errMsg, e);
-                throw new SparqlProxyException(errMsg, e);
-            }
-        } else if(queries.get(id).queryType.equals(SparqlQuery.TYPE.GRAPH)) {
-            GraphQueryResult queryResult;
-            try {
-                queryResult = ((GraphQuery) query).evaluate();
-            } catch (QueryEvaluationException e) {
-                final String errMsg = "Error during the evaluation of the SPARQL query";
-                LOGGER.error(errMsg, e);
-                throw new SparqlProxyException(errMsg, e);
-            }
-            try {
-                statements = filter.getStatements(queryResult, queries.get(id).queryType);
-            } catch (FilterException e) {
-                final String errMsg = "Error while inspecting the query result";
-                LOGGER.error(errMsg, e);
-                throw new SparqlProxyException(errMsg, e);
-            }
-            try {
-                queryResult.close();
-            } catch (QueryEvaluationException e) {
-                final String errMsg = "Error while closing queryresult";
-                LOGGER.error(errMsg, e);
-                throw new SparqlProxyException(errMsg, e);
-            }
-        }
-        releaseConnection(repositoryConnection);
+        return query;
+    }
+
+    private Ranker getRanker(QueryRecord queryRecord) throws SparqlProxyException {
+
+        Ranker ranker;
         try {
-            return ranker.rank(filter.getObjects(statements, this.rdfizer));
-        } catch (FilterException e) {
-            final String errMsg = "Error while filtering query results";
+            ranker = queryRecord.getRanker().newInstance();
+        } catch (InstantiationException e) {
+            final String errMsg = "Error while instantiating the provided ranker";
+            throw new SparqlProxyException(errMsg, e);
+        } catch (IllegalAccessException e) {
+            final String errMsg = "Error while accessing the provided ranked";
+            throw new SparqlProxyException(errMsg, e);
+        }
+        return ranker;
+    }
+
+    private Filter getFilter(QueryRecord queryRecord) throws SparqlProxyException {
+
+        Filter filter;
+        try {
+            filter = queryRecord.getFilter().newInstance();
+        } catch (InstantiationException e) {
+            final String errMsg = "Error while instantiating filter";
             LOGGER.error(errMsg, e);
             throw new SparqlProxyException(errMsg, e);
-        } catch (RankerException e) {
-            final String errMsg = "Error while ranking query results";
+        } catch (IllegalAccessException e) {
+            final String errMsg = "Error while accessing to filter";
             LOGGER.error(errMsg, e);
             throw new SparqlProxyException(errMsg, e);
         }
+        return filter;
     }
 
     protected void releaseConnection(RepositoryConnection repositoryConnection)
@@ -235,50 +307,50 @@ public class SesameVirtuosoSparqlProxy implements SparqlProxy {
         }
     }
 
-    private class QueryRecord {
-
-        private String queryTemplate;
-
-        private SparqlQuery.TYPE queryType;
-
-        private Class<? extends Filter> filter;
-
-        private Class<? extends Ranker> ranker;
-
-        public QueryRecord(
-                String queryTemplate,
-                SparqlQuery.TYPE queryType,
-                Class<? extends Filter> filter,
-                Class<? extends Ranker> ranker
-        ) {
-            this.queryTemplate = queryTemplate;
-            this.queryType = queryType;
-            this.filter = filter;
-            this.ranker = ranker;
-        }
-
-        public String getQueryTemplate() {
-            return queryTemplate;
-        }
-
-        public Query prepareQuery(
-                RepositoryConnection repositoryConnection,
-                String queryInstance) throws MalformedQueryException, RepositoryException {
-            if (queryType.equals(SparqlQuery.TYPE.TUPLE)) {
-                return repositoryConnection.prepareTupleQuery(QueryLanguage.SPARQL, queryInstance);
-            }
-            if (queryType.equals(SparqlQuery.TYPE.GRAPH)) {
-                return repositoryConnection.prepareGraphQuery(QueryLanguage.SPARQL, queryInstance);
-            }
-            throw new IllegalArgumentException("Unable to instantiate a proper query");
-        }
-
-        public Class<? extends Filter> getFilter() {
-            return filter;
-        }
-
-        public Class<? extends Ranker> getRanker() {
-            return ranker;
-        }
-    }
+//    private class QueryRecord {
+//
+//        private String queryTemplate;
+//
+//        private SparqlQuery.TYPE queryType;
+//
+//        private Class<? extends Filter> filter;
+//
+//        private Class<? extends Ranker> ranker;
+//
+//        public QueryRecord(
+//                String queryTemplate,
+//                SparqlQuery.TYPE queryType,
+//                Class<? extends Filter> filter,
+//                Class<? extends Ranker> ranker
+//        ) {
+//            this.queryTemplate = queryTemplate;
+//            this.queryType = queryType;
+//            this.filter = filter;
+//            this.ranker = ranker;
+//        }
+//
+//        public String getQueryTemplate() {
+//            return queryTemplate;
+//        }
+//
+//        public Query prepareQuery(
+//                RepositoryConnection repositoryConnection,
+//                String queryInstance) throws MalformedQueryException, RepositoryException {
+//            if (queryType.equals(SparqlQuery.TYPE.TUPLE)) {
+//                return repositoryConnection.prepareTupleQuery(QueryLanguage.SPARQL, queryInstance);
+//            }
+//            if (queryType.equals(SparqlQuery.TYPE.GRAPH)) {
+//                return repositoryConnection.prepareGraphQuery(QueryLanguage.SPARQL, queryInstance);
+//            }
+//            throw new IllegalArgumentException("Unable to instantiate a proper query");
+//        }
+//
+//        public Class<? extends Filter> getFilter() {
+//            return filter;
+//        }
+//
+//        public Class<? extends Ranker> getRanker() {
+//            return ranker;
+//        }
+//    }
 }
