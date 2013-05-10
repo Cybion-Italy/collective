d14e48be294579dbb90cbe436e6f82d60860da6c
Matteo Moci <mox601@libero.it>
2013-05-10 10:50:02 +0200

    added files for spike

---
+++ profile-manager/profiling-line-api/src/test/java/com/collective/profiler/line/exampleitems/LanguageDetectionItem.java
@@ -0,0 +1,8 @@
+package com.collective.profiler.line.exampleitems;
+
+/**
+ * @author Matteo Moci ( matteo (dot) moci (at) gmail (dot) com )
+ */
+public class LanguageDetectionItem {
+
+}
---
+++ profile-manager/profiling-line-api/src/test/java/com/collective/profiler/line/model/SentimentedTweet.java
@@ -0,0 +1,8 @@
+package com.collective.profiler.line.model;
+
+/**
+ * @author Matteo Moci ( matteo (dot) moci (at) gmail (dot) com )
+ */
+public class SentimentedTweet {
+
+}
